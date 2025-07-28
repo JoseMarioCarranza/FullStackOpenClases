792328282d4ec58b2bbaab061a48d444dd754314
JoseMarioCarranza <imt_josecarranza@outlook.com>
2025-07-27 21:17:20 -0700

    Do not render objects

--- Part1/example1/src/App.jsx
+++ Part1/example1/src/App.jsx
@@ -1,26 +1,10 @@
-const Hello = ({ name, age }) => {
-  return (
-    <div>
-      <p>Hello {name}, you are {age} years old</p>
-    </div>
-  )
-}
-
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by <a href="https://github.com/JoseMarioCarranza">Aperta</a>
-    </div>
-  )
-}
-
 function App() {
 
+  const friends = ['Peter', 'Maya']
+
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name='José' age={11 + 19} />
-      <Footer />
+      <p>{friends}</p>
     </div>
   )
 }
