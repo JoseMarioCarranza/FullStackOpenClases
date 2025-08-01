c91580fc5ec255350f9e97fcfccf006bc5fe2da1
JoseMarioCarranza <imt_josecarranza@outlook.com>
2025-07-31 17:41:44 -0700

    Cleaner Syntax for Defining Object Literals

--- Part2/example2/src/services/notes.js
+++ Part2/example2/src/services/notes.js
@@ -17,8 +17,4 @@ const update = (id, newObject) => {
     return request.then(r => r.data)
 }
 
-export default {
-    getAll: getAll,
-    create: create,
-    update: update
-}
+export default { getAll, create, update }
