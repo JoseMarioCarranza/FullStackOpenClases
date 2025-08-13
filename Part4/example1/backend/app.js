54dd9384aada662b292e8b70ae39793107ad32cd
JoseMarioCarranza <imt_josecarranza@outlook.com>
2025-08-13 00:38:11 -0700

    Token authentication

--- Part4/example1/backend/app.js
+++ Part4/example1/backend/app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 mongoose.set('strictQuery', false)
 
@@ -27,6 +28,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.errorHandler)
 app.use(middleware.unknownEndpoint)
