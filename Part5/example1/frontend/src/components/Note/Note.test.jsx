d9ce86d5eee0fae34480796b3800387f8b6cfdd9
JoseMarioCarranza <imt_josecarranza@outlook.com>
2025-08-21 23:28:22 -0700

    Searching for content in a component

--- Part5/example1/frontend/src/components/Note/Note.test.jsx
+++ Part5/example1/frontend/src/components/Note/Note.test.jsx
@@ -7,8 +7,9 @@ test('renders content', () => {
         important: true
     }
 
-    render(<Note note={note} />)
-
-    const element = screen.getByText('Component testing is done with react-testing-library')
-    expect(element).toBeDefined()
+    const { container } = render(<Note note={note} />)
+    const div = container.querySelector('.note')
+    expect(div).toHaveTextContent(
+        'Component testing is done with react-testing-library'
+    )
 })
