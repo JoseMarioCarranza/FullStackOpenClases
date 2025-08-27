08be5984558c7b0fe30a0a18826010255e55e145
JoseMarioCarranza <imt_josecarranza@outlook.com>
2025-08-26 18:38:02 -0700

    Writing on the form

--- Part5/example1/playwright/tests/note_app.spec.js
+++ Part5/example1/playwright/tests/note_app.spec.js
@@ -1,9 +1,25 @@
-const { test, expect } = require('@playwright/test')
+const { test, expect, describe, beforeEach } = require('@playwright/test')
 
-test('front page can be opened', async ({ page }) => {
-    await page.goto('http://localhost:5173/')
+describe('Note app', () => {
 
-    const locator = await page.getByText('Notes')
-    await expect(locator).toBeVisible()
-    await expect(page.getByText('Note app, made by aperta 2024')).toBeVisible()
+    beforeEach(async ({ page }) => {
+        await page.goto('http://localhost:5173/')
+    })
+
+    test('front page can be opened', async ({ page }) => {
+        const locator = await page.getByText('Notes')
+        await expect(locator).toBeVisible()
+        await expect(page.getByText('Note app, made by aperta 2025')).toBeVisible()
+    })
+
+    test('login form can be opened', async ({ page }) => {
+
+        await page.getByRole('button', { name: 'login' }).click()
+
+        await page.getByTestId('username').fill('Aperta')
+        await page.getByTestId('password').fill('123456')
+        await page.getByRole('button', { name: 'login' }).click()
+
+        await expect(page.getByText('José logged-in')).toBeVisible()
+    })
 })
